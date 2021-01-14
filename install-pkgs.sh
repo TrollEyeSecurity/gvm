f386553290dcea1501f4e45bad093032ef8cf1c1
Avery Rozar <avery.rozar@trolleyesecurity.com>
2021-01-14 16:31:24 -0500

    remove extra code

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 export NON_INT=1
-dnf install 'dnf-command(config-manager)' epel-release wget -y && dnf config-manager --set-enabled powertools && wget -q -O - https://updates.atomicorp.com/installers/atomic | sh && dnf update -y && \
- { cat <<EOF
-gvm
-openssh-server.x86_64
-EOF
-} | xargs dnf install -y
+dnf install 'dnf-command(config-manager)' epel-release wget -y && \
+    dnf config-manager --set-enabled powertools && \
+    wget -q -O - https://updates.atomicorp.com/installers/atomic | sh && \
+    dnf update -y && \
+    dnf install -y gvm openssh-server.x86_64
