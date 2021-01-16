560806e1e8c02068a1a899c6d18e3e95318e0b08
Avery Rozar <avery.rozar@trolleyesecurity.com>
2021-01-16 13:32:45 -0500

    modify gvmd socket, add GSAD

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,12 +9,6 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' && ssh-keygen -t dsa -f /et
 #Set sysctl
 sysctl -w net.core.somaxconn=1024
 sysctl vm.overcommit_memory=1
-if ! grep -q "net.core.somaxconn=1024" /etc/sysctl.conf; then
-	echo "net.core.somaxconn=1024"  >> /etc/sysctl.conf
-fi
-if ! grep -q "vm.overcommit_memory=1" /etc/sysctl.conf; then
-	echo "vm.overcommit_memory=1" >> /etc/sysctl.conf
-fi
 
 # Start Redis, PostgreSQL, and OSPd
 ./start_redis.sh
@@ -23,14 +17,18 @@ fi
 
 # Start the core GVM processes (NO UI - GSA)
 echo "Starting Greenbone Vulnerability Manager..."
-su -c "gvmd --unix-socket=/tmp/gvmd.sock --osp-vt-update=/run/ospd/ospd.sock" gvm
+su -c "gvmd" gvm
 
-while  [[ ! -S /tmp/gvmd.sock ]]; do
+while  [[ ! -S /var/run/gvm/gvmd.sock ]]; do
 	sleep 1
 done
 
 # make sure gmv-cli can access the gvmd.sock
-chmod 666 /tmp/gvmd.sock
+chmod 770 /var/run/gvm/gvmd.sock
+
+# Make sure root is not greedy
+chown gvm:gvm -R /var/run/ospd/
+chown gvm:gvm -R /var/run/gvm/
 
 # Add GVM admin user
 until su -c "gvmd --get-users" gvm; do
@@ -41,5 +39,9 @@ su -c "gvmd --create-user=${USERNAME} --password=${PASSWORD}" gvm
 admin_user_uuid=$(su -c "gvmd --get-users --verbose| cut -d\" \" -f 2" gvm)
 su -c "gvmd --modify-setting 78eceaec-3385-11ea-b237-28d24461215b --value ${admin_user_uuid}" gvm
 
-# Spew all the logs
+# Start GSAD
+echo "Starting GSAD..."
+/usr/sbin/gsad --munix-socket=/var/run/gvm/gvmd.sock
+
+## Spew all the logs
 tail -F /var/log/gvm/*
